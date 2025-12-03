e3fe7a517185d85a3c45bbd4f1969cba27ebe2ec
Robin Mauritz <robin@mdev.co.id>
2025-12-03 10:37:02 +0700

    feat: Add mimalloc option (default true)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use color_eyre::Report;
 
 mod init;
 {%- endif %}
+{%- if use_mimalloc %}
+
+// Avoid musl's default allocator due to lackluster performance
+// https://nickb.dev/blog/default-musl-allocator-considered-harmful-to-performance
+#[cfg(target_env = "musl")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+{%- endif %}
 
 #[tracing::instrument]
 fn main() -> Result<(), Report> {
