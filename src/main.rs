876954aefe593794c4ead1c422b0018a7053f89e
Robin Mauritz <robinmauritzrm@gmail.com>
2026-06-18 10:20:04 +0700

    chore: Tweak templating issue

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use color_eyre::Report;
 {%- if use_init %}
-
 mod init;
-{%- endif %}
+{%- endif -%}
 {%- if use_mimalloc %}
 
 // Avoid musl's default allocator due to lackluster performance
@@ -14,9 +13,9 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[tracing::instrument]
 fn main() -> Result<(), Report> {
-{%- if use_init %}
+    {%- if use_init %}
     init::initialize()?;
-{% endif %}
+    {%- endif %}
     println!("Hello, world!");
 
     Ok(())
